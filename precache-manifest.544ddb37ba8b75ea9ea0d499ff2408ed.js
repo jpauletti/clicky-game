b4965dddfd7b2338395cb10b6fe5aeefad74008e
Julia Pauletti <julia.pauletti@yahoo.com>
2019-07-19 18:08:49 -0500

    Updates

---
+++ precache-manifest.544ddb37ba8b75ea9ea0d499ff2408ed.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae8f453403ef20ef22e4f3c215e84ce",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "cb3f90917adef559c600",
+    "url": "/clicky-game/static/css/main.e2b9a295.chunk.css"
+  },
+  {
+    "revision": "c4d2d927e18841549307",
+    "url": "/clicky-game/static/js/2.5fd5d7c0.chunk.js"
+  },
+  {
+    "revision": "cb3f90917adef559c600",
+    "url": "/clicky-game/static/js/main.8f320456.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "44a286c06d8eaaf7a8ce9589f3376b9f",
+    "url": "/clicky-game/static/media/draco-malfoy.44a286c0.jpg"
+  },
+  {
+    "revision": "830a6409d9fef2968329c70657c31afe",
+    "url": "/clicky-game/static/media/dumbledore.830a6409.jpg"
+  },
+  {
+    "revision": "287cc8d6906782635d385b5ce82fac38",
+    "url": "/clicky-game/static/media/fleur.287cc8d6.jpg"
+  },
+  {
+    "revision": "1ff24ca83d38d5bf8c42034e414ec52f",
+    "url": "/clicky-game/static/media/harry-potter.1ff24ca8.jpg"
+  },
+  {
+    "revision": "1293f3c451c90ef8216bc4bce4d5fd4e",
+    "url": "/clicky-game/static/media/hermione-granger.1293f3c4.jpg"
+  },
+  {
+    "revision": "4439e8aeed4354d3deed6436d96a5e1f",
+    "url": "/clicky-game/static/media/luna.4439e8ae.jpg"
+  },
+  {
+    "revision": "01124fbf202f951928eef9bf19ebbc0f",
+    "url": "/clicky-game/static/media/mad-eye.01124fbf.jpg"
+  },
+  {
+    "revision": "bbd58fba45f0b428390265174c7216bd",
+    "url": "/clicky-game/static/media/neville-longbottom.bbd58fba.jpg"
+  },
+  {
+    "revision": "e69fe2eb8dec7937d7f66508e55a5d54",
+    "url": "/clicky-game/static/media/remus-lupin.e69fe2eb.jpg"
+  },
+  {
+    "revision": "ff61c0af26900e1b4c2f9a82b84d0c7d",
+    "url": "/clicky-game/static/media/ron-weasley.ff61c0af.jpg"
+  },
+  {
+    "revision": "8d7a683731581187225ab86ae443e59a",
+    "url": "/clicky-game/static/media/snape.8d7a6837.jpg"
+  },
+  {
+    "revision": "ed3d7e6b7fabb77574e33704a83fabd9",
+    "url": "/clicky-game/static/media/tonks.ed3d7e6b.jpg"
+  }
+]);
